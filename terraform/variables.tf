29bb16bff014a875301ca7c30a795b6091028b68
Ferb005 <frd199315@gmail.com>
2026-06-20 01:06:29 -0400

    feat: add terraform config for azure cdn, blob, log analytics

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,16 @@
-# In this file put the variables related to the deployment
-variable "variable_name" {
-    type = "type",
-    description = "Description"
+variable "environment" {
+  type        = string
+  description = "Environment name: devel or stage"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region"
+  default     = "eastus"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Project name prefix for resources"
+  default     = "rdicidr"
 }
